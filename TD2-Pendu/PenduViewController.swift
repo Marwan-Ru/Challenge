5ddda0a71398f142d5e21fb1ca52ce5ce59da49d
Woople74 <aitaddi.marwan@gmail.com>
2024-03-19 14:50:36 +0100

    Initial commit

---
+++ TD2-Pendu/PenduViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PenduViewController.swift
+//  TD2-Pendu
+//
+//  Created by Marwan Ait Addi on 19/03/2024.
+//
+
+import UIKit
+
+class PenduViewController: UIViewController {
+
+    var niveau : Int = 1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
